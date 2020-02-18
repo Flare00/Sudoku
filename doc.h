23a3476138c32286cf3dd1ffc03609429a79ca55
Charlister <Charlister22@gmail.com>
2020-02-18 12:41:53 +0100

    nouveaux exemples de grilles

--- doc.h
+++ doc.h
@@ -11,15 +11,15 @@
 *	- indice -> valeur déjà inscrite dans la grille à l'initialisation.
 *	- candidats -> valeurs possibles pour une cellule donnée.
 *
-*	- candidats célibataire -> quand il n'y as q'un seul candidat possible.
-*	- candidats célibataire caché -> quand il n'es pas le seul candidats pour une cellule, mais que sa présence est unique dans un groupe.
+*	- candidat célibataire -> quand il n'y as q'un seul candidat possible.
+*	- candidats célibataire caché -> quand il n'est pas le seul candidat pour une cellule, mais que sa présence est unique dans un groupe.
 *
 * \see http://sudoku.cristallogenese.com/guide/guidesudoku.htm#grid 
 *
 *	---Bitboard---
 *	Pour le projet nous utiliserons la technique du Bitboard.
 * 	le Bitboard consiste a representé la présence d'un element dans une ligne, colone ou région, dans une suites de bits.
-*	(ex : un ligne 1, ayant un 1, un 4 et un 9. 
+*	(ex : la première ligne ayant un 1, un 4 et un 9. 
 *		correspondance	|1|2|3|4|5|6|7|8|9|
 *		bits			|1|0|0|1|0|0|0|0|1| 
 *		Cette representation ne montre que la présence d'un chiffre, et non son emplacement, sa position pourra être retrouvé en utilisant les 3 bitboard, ligne/colonne/région.)
