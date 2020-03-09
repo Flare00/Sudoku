af29c9a035f9245d9db6ca2b3e22bacce49b6ab4
Flareden <55061569+Flare00@users.noreply.github.com>
2020-03-10 00:11:59 +0100

    Modif/Ajout fonctions/conversion camelCase
    Conversion des nom de fonctions et variable en respectant camelCase.
    Renommage de retirerCaseRandom en retirerCaseRandomAvecCaseRestante
    Ajout des fonctions : 
      - permutationLignes
      - permutationColones
      - rotation
      - retirerCaseRandom //nouveau
    Implementation : 
      - retirerCaseRandom //nouveau
      - permutationLignes
      - permutationColones

--- generation.c
+++ generation.c
@@ -2,26 +2,26 @@
 #include "difficulte.h"
 
 // A COMPLETER
-unsigned int *generer_chaine_sudoku(size_t taille, int level){
+unsigned int *genererChaineSudoku(size_t taille, int level){
     int difficulte = 0;
-    uint8_t ** grille = generer_grille_complete(taille);
+    uint8_t ** grille = genererGrilleComplete(taille);
 
-    int cases_restante = retirer_case_random(grille);
+    int casesRestante = retirerCaseRandom(grille);
 
     do{
-        uint8_t ** grille_temp = grille; // faire une de la grille copie;
-        cases_restante = retirer_case_random(grille_temp);
-        difficulte = validiter_et_difficulter(grille, taille, level);
+        uint8_t ** grilleTemp = grille; // faire une de la grille copie;
+        casesRestante = retirerCaseRandomAvecCaseRestante(grilleTemp, taille);
+        difficulte = validiterEtDifficulter(grille, taille, level);
         //La difficulté 5 permet a l'algorithme de renvoyer la derniere grille, car la nouvelle ne correspond plus au niveau souhaiter.
         //La difficulté 0 (erreur) ne remplace pas grille par grille_temp et permet donc de changer la case retirer aléatoirement.
         if (difficulte != 5 && difficulte != 0){
             // libere la mémoire de la grille, et la remplace par la grille temporaire.
             free(grille); 
-            grille = grille_temp;
+            grille = grilleTemp;
         }
 
         //si le nombre de cases restante est inferieur a 20, termine la boucle.
-        if(cases_restante < 20){
+        if(casesRestante < 20){
             difficulte = 5;
         }
     }while(difficulte != 5);
@@ -29,26 +29,27 @@ unsigned int *generer_chaine_sudoku(size_t taille, int level){
     return ""; // A COMPLETER
 }
 
-uint8_t **generer_grille_sudoku_valide(int taille, int level){
+uint8_t **genererGrilleSudokuValide(int taille, int level){
     int difficulte = 0;
-    uint8_t ** grille = generer_grille_complete(taille);
+    uint8_t ** grille = genererGrilleComplete(taille);
 
-    int cases_restante = retirer_case_random(grille);
+    int casesRestante = retirerCaseRandom(grille);
 
     do{
-        uint8_t ** grille_temp = grille; // faire une de la grille copie;
-        cases_restante = retirer_case_random(grille_temp);
-        difficulte = validiter_et_difficulter(grille, taille, level);
+        uint8_t ** grilleTemp = grille; // faire une de la grille copie;
+        casesRestante = retirerCaseRandomAvecCaseRestante(grilleTemp, taille);
+        difficulte = validiterEtDifficulter(grilleTemp, taille, level);
         //La difficulté 5 permet a l'algorithme de renvoyer la derniere grille, car la nouvelle ne correspond plus au niveau souhaiter.
         //La difficulté 0 (erreur) ne remplace pas grille par grille_temp et permet donc de changer la case retirer aléatoirement.
         if (difficulte != 5 && difficulte != 0){
             // libere la mémoire de la grille, et la remplace par la grille temporaire.
             free(grille); 
-            grille = grille_temp;
+            grille = grilleTemp;
+            free(grilleTemp);
         }
 
         //si le nombre de cases restante est inferieur a 20, termine la boucle.
-        if(cases_restante < 20){
+        if(casesRestante < 20){
             difficulte = 5;
         }
     }while(difficulte != 5);
@@ -56,27 +57,68 @@ uint8_t **generer_grille_sudoku_valide(int taille, int level){
     return grille;
 }
 
-size_t retirer_case_random(uint8_t ** grille, size_t taille){
-    size_t nb_cases = 0;
-    struct pos_case * pos = malloc(taille*taille);
+size_t retirerCaseRandomAvecCaseRestante(uint8_t ** grille, size_t taille){
+    size_t nbCases = 0;
+    struct posCase * pos = malloc(taille*taille);
     for(int i = 0; i < taille; i++){
         for(int j = 0; j < taille; j++){
             if(grille[i][j] != 0){
-                struct pos_case temp = {i,j};
+                struct posCase temp = {i,j};
                 pos[nb_cases] = temp;
                 nb_cases++;
             }
         }
     }
 
-    int a_suppr = rand(time())%nb_cases;
+    int aSuppr = rand(time())%nb_cases;
 
-    grille[pos_case[a_suppr].colonne][pos_case[a_suppr].ligne] = 0;
+    grille[pos[aSuppr].colonne][pos[aSuppr].ligne] = 0;
 
-    return (nb_cases - 1);
+    return (nbCases - 1);
 }
 
-uint8_t generer_grille_complete(size_t taille){
+uint8_t retirerCaseRandom(uint8_t ** grille, size_t taille){
+    size_t ligne = rand(time())%taille;
+    size_t colonne = rand(time())%taille;
+    int reussi = 0;
+
+    while (reussi == 0){
+        if(grille[colonne][ligne] != 0){
+            grille[colonne][ligne] = 0;
+            reussi = 1;
+        }
+    }
+
+    return grille;
+}
+
+
+uint8_t genererGrilleComplete(size_t taille){
     // A FAIRE;
+    return 0;
+}
+
+uint8_t ** permutationLignes(uint8_t ** grille, size_t taille, size_t ligne1, size_t ligne2){
+        for(int i = 0; i < taille; i++){
+            int8_t temp = grille[i][ligne1];
+            grille[i][ligne1] = grille[i][ligne2];
+            grille[i][ligne2] = temp;
+        }
+
+        return grille;
+}
+
+uint8_t ** permutationColones(uint8_t ** grille, size_t taille, size_t colone1, size_t colone2){
+        for(int i = 0; i < taille; i++){
+            int8_t temp = grille[colone1][i];
+            grille[colone1][i] = grille[colone2][i];
+            grille[colone2][i] = temp;
+        }
+
+        return grille;
+}
+
+uint8_t ** rotation(uint8_t ** grille, size_t taille){
+    //A FAIRE;
     return 0;
 }
--- generation.h
+++ generation.h
@@ -4,40 +4,75 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-struct pos_case 
-{
-    size_t colonne;
+struct posCase{
     size_t ligne;
-};
+    size_t colonne;
+}
 
 /**
 * \brief Génère chaine correspondant à une grille de sudoku Valide et de difficulté donnée.
-* \param int taille : taille de la grille de sudoku
-* \param int level : le niveau de difficulté nécéssaire pour la grille
+* \param taille : taille de la grille de sudoku
+* \param level : le niveau de difficulté nécéssaire pour la grille
 * \return unisgned int * : chaine correspondant à la grille de sudoku.
 */
-unsigned int *generer_chaine_sudoku(size_t taille, int level);
+uint8_t * genererChaineSudoku(size_t taille, int level);
 
 /**
 * \brief Génère une grille de sudoku valide et de difficulté donnée.
-* \param int taille : taille de la grille de sudoku
-* \param int level : le niveau de difficulté nécéssaire pour la grille
+* \param taille size_t : taille de la grille de sudoku
+* \param level int : le niveau de difficulté nécéssaire pour la grille
 * \return uint8_t ** : la grille de sudoku.
 */
-uint8_t **generer_grille_sudoku(size_t taille, int level);
+uint8_t **genererGrilleSudoku(size_t taille, int level);
 
 /**
  * \brief retire une case, parmi les cases non vide, de la grille de façon aléatoire.
- * \param uint8_t grille grille : grille du sudoku 
+ * \param grille : grille du sudoku 
  * \return nombre de cases restante.
  */ 
-size_t retirer_case_random(uint8_t ** grille, size_t taille);
+size_t retirerCaseRandomAvecCaseRestante(uint8_t ** grille, size_t taille);
+
+/**
+ * \brief retire une case, parmi les cases non vide, de la grille de façon aléatoire.
+ * \param grille : grille du sudoku 
+ * \return grille de sudoku.
+ */ 
+uint8_t ** retirerCaseRandom(uint8_t ** grille, size_t taille);
 
 /**
  * \brief genere une grille de sudoku complete.
- * \param size_t taille : taille de la grille.
+ * \param taille : taille de la grille.
  * \return grille de sudoku complete.
  */
-uint8_t generer_grille_complete(size_t taille);
+uint8_t genererGrilleComplete(size_t taille);
+
+/**
+ * \brief permute deux lignes de la grille de sudoku donnée.
+ * \param grille : grille du sudoku.
+ * \param taille : taille de la grille.
+ * \param ligne1 : ligne à permuter.
+ * \param ligne2 : ligne à permuter.
+ * \return grille de sudoku permuté.
+ */
+uint8_t ** permutationLignes(uint8_t ** grille, size_t taille, size_t ligne1, size_t ligne2);
+
+/**
+ * \brief permute deux lignes de la grille de sudoku donnée.
+ * \param grille : grille du sudoku.
+ * \param taille : taille de la grille.
+ * \param colone1 : colone à permuter.
+ * \param colone2 : colone à permuter.
+ * \return grille de sudoku permuté.
+ */
+uint8_t ** permutationColones(uint8_t ** grille, size_t taille, size_t colone1, size_t colone2);
+
+/**
+ * \brief permute deux lignes de la grille de sudoku donnée.
+ * \param grille : grille du sudoku.
+ * \param taille : taille de la grille.
+ * \return grille de sudoku permuté.
+ */
+uint8_t ** rotation(uint8_t ** grille, size_t taille);
+
 
 #endif
