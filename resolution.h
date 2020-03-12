660c95bbfa053397220370af7c170e0847985685
PupsDev <41515521+PupsDev@users.noreply.github.com>
2020-03-12 17:58:27 +0100

    test

--- resolution.h
+++ resolution.h
@@ -15,6 +15,14 @@
 Liste* adjonctionEnTete(Liste *liste, uint64_t candidats, size_t i, size_t j);
 Liste* creerListeUniqueCandidat(size_t n, uint64_t *bbL, uint64_t *bbC, uint64_t *bbB, uint8_t** grille);
 void heuristiqueUniqueCandidat(size_t n, uint64_t *bbL, uint64_t *bbC, uint64_t *bbB, uint8_t** grille);
+/** @brief Vérifie si la grille est résolu
+  * @param Liste *dl : pointeur sur une liste
+  * @param uint64_t* bbL : bitBoard ligne
+  * @param uint64_t* bbC : bitBoard colonne
+  * @param uint64_t* bbB : bitBoard block
+  * @param uint8_t** grille : pointeur sur la grille
+  * @param uint8_t** map : tableau contenant les indices de région pour le tableau de bits block (bbB)
+  */
 void heuristiquePaireCachee(size_t taille,Liste *dl,uint64_t *bbL, uint64_t *bbC, uint64_t *bbB,uint8_t**map);
 
 /** @brief Vérifie si la grille est résolu
