5d7b289b9ac072bb3b214c87450b56bf24c78aeb
Khelian <abraxaslegacy@yahoo.fr>
2020-03-13 11:03:40 +0100

    menu v1.1

--- interfaceGraphique/interfacegtk.c
+++ interfaceGraphique/interfacegtk.c
@@ -9,29 +9,42 @@ static void print_hello (GtkWidget *widget, gpointer data){
   g_print ("Hello World\n");
 }
 
-// Fonction Menu
-static void createMenu(){
-    // Widgets
-    GObject *fenetre;
-    GObject *etiquetteTitre;
-    GObject *boutonJouer;
-    GObject *boutonRegles;
-    GObject *boutonOptions;
-    GObject *boutonQuitter;
+gboolean hide(GtkWidget *button, GdkEventButton *event, gpointer data)
+{
+	f_Menu *my_m = (f_Menu*) data;
 
-    // Déclarations
-    fenetre = gtk_builder_get_object (constructeurMenu, "fenetre");
-    etiquetteTitre = gtk_builder_get_object (constructeurMenu, "etiquetteTitre");
-    boutonJouer = gtk_builder_get_object (constructeurMenu, "boutonJouer");
-    boutonRegles = gtk_builder_get_object (constructeurMenu, "boutonRegles");
-    boutonOptions = gtk_builder_get_object (constructeurMenu, "boutonOptions");
-    boutonQuitter = gtk_builder_get_object (constructeurMenu, "boutonQuitter");
+  gtk_widget_hide(GTK_WIDGET (my_m->boutonJouer));
+  gtk_widget_hide(GTK_WIDGET (my_m->boutonRegles));
+  gtk_widget_hide(GTK_WIDGET (my_m->boutonOptions));
+  gtk_widget_hide(GTK_WIDGET (my_m->boutonQuitter));
 
-    // Signaux
-    g_signal_connect (fenetre, "destroy", G_CALLBACK (gtk_main_quit), NULL);
-    g_signal_connect (boutonJouer, "clicked", G_CALLBACK (print_hello), NULL);
-    g_signal_connect (boutonQuitter, "clicked", G_CALLBACK (gtk_main_quit), NULL);
+	return FALSE;
 }
+
+// Fonction Masquage Menu
+// static void menuCacher(f_Menu *leMenu){
+//     gtk_widget_hide(leMenu->boutonJouer);
+// }
+
+// Fonction Création Menu
+static void menuCreer(f_Menu *leMenu){
+  // Déclarations
+  leMenu->fenetre = gtk_builder_get_object (constructeurMenu, "fenetre");
+  leMenu->etiquetteTitre = gtk_builder_get_object (constructeurMenu, "etiquetteTitre");
+  leMenu->boutonJouer = gtk_builder_get_object (constructeurMenu, "boutonJouer");
+  leMenu->boutonRegles = gtk_builder_get_object (constructeurMenu, "boutonRegles");
+  leMenu->boutonOptions = gtk_builder_get_object (constructeurMenu, "boutonOptions");
+  leMenu->boutonQuitter = gtk_builder_get_object (constructeurMenu, "boutonQuitter");
+
+  // Signaux
+  g_signal_connect (leMenu->fenetre, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+  g_signal_connect (leMenu->boutonJouer, "clicked", G_CALLBACK (print_hello), NULL);
+  g_signal_connect (leMenu->boutonRegles, "clicked", G_CALLBACK (hide), leMenu);
+  g_signal_connect (leMenu->boutonOptions, "clicked", G_CALLBACK (print_hello), NULL);
+  g_signal_connect (leMenu->boutonQuitter, "clicked", G_CALLBACK (gtk_main_quit), NULL);
+
+}
+
 
 
 int main (int argc, char *argv[]){
@@ -39,6 +52,7 @@ int main (int argc, char *argv[]){
   // Initialisation
   gtk_init (&argc, &argv);
 
+  f_Menu *leMenu = (f_Menu*) malloc (sizeof (f_Menu));
   // Vérification du constructeurMenu (constructeurMenu.ui)
   constructeurMenu = gtk_builder_new ();
   if (gtk_builder_add_from_file (constructeurMenu, "builder.ui", &error) == 0){
@@ -48,7 +62,7 @@ int main (int argc, char *argv[]){
   }
 
   // Menu
-  createMenu();
+  menuCreer(leMenu);
 
   // Main Fonction GTK
   gtk_main ();
--- interfaceGraphique/interfacegtk.h
+++ interfaceGraphique/interfacegtk.h
@@ -3,10 +3,24 @@
 
 #include <gtk/gtk.h>
 
+typedef struct menu {
+  GObject *fenetre;
+  GObject *etiquetteTitre;
+  GObject *boutonJouer;
+  GObject *boutonRegles;
+  GObject *boutonOptions;
+  GObject *boutonQuitter;
+} f_Menu;
+
 /**
 * \brief Création de la fenêtre GTK Menu.
 */
-static void createMenu();
+static void menuCreer(f_Menu *leMenu);
+
+/**
+* \brief Création de la fenêtre GTK Menu.
+*/
+static void menuCacher(f_Menu *leMenu);
 
 
 #endif
