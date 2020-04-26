7077cdbf267be7238d476e3a3bc5ec1f391b8166
Flareden <55061569+Flare00@users.noreply.github.com>
2020-04-26 23:59:22 +0200

    Modification et ajout de documentation
    Modification des appel de fonction.
    ajout de documentation et commentaires.
    : ne fait toujours pas la vérification de la validité du sudoku pour taille <= 16.

--- difficulte.c
+++ difficulte.c
@@ -6,8 +6,8 @@ int validiterEtDifficulter(uint8_t **grille, size_t taille, int level)
 
     affichage(grille, taille);
     affichage2(grille,taille);
-    
-    if(taille <= 0){ //Bug
+    //si la taille est <= 16 alors 
+    if(taille <= 0){
         int n = sqrt(taille);
         uint8_t **map = mapCreer(n);
         if (!map){
@@ -16,24 +16,27 @@ int validiterEtDifficulter(uint8_t **grille, size_t taille, int level)
         uint32_t *bbL = bitboard32Creer(taille);
         uint32_t *bbC = bitboard32Creer(taille);
         uint32_t *bbB = bitboard32Creer(taille);
-
+        //initialise les bitboard
         bitBoardInitialiser32(grille, n, bbL, bbC, bbB);
-        Liste32 *liste = listeGenerer32(n, bbL, bbC, bbB, grille, map);
-
+        //initialise la liste
+        Liste32 *liste = liste32Generer(n,grille, bbL, bbC, bbB, map);
+        //si le nombre de resolution est de 1 alors
         if (resolu_32(grille, liste, bbL, bbC, bbB,map, 0, liste->i, liste->j) == 1)
         {
+            //determine la difficulté.
             retour = difficulter(grille, taille, level);
         }
-
+        //detruit les elements alloué
         listeDetruire(liste);
         bitBoardDetruire(bbL);
         bitBoardDetruire(bbC);
         bitBoardDetruire(bbB);
         detruire2D(map, taille);
     } else {
+        //sinon determine juste la difficulté.
         retour = difficulter(grille, taille, level);
     }
-
+    //detruit la grille temporaire.
     detruire2D(grille, taille);
     return retour;
 }
@@ -44,46 +47,54 @@ int difficulter(uint8_t **grille, size_t taille, int level)
     size_t n = sqrt(taille);
 
     uint8_t ** map = mapCreer(n);
-
+    //genere la liste
     Liste *liste = listeGenerer(n, grille, map);
-
+    //execute les heurisitque facile
     if (heuristiqueFacile(grille, n, liste, map) == 0)
     {
+        //si le niveau eneleve toutes les case retourne le niveau
         retour = 1;
     }
     else
     {
         if (level == 1)
         {
-
+            //sinon si le level choisi est celui la retourne que le niveau est dépassé
             retour = 5;
         }
         else
         {
+            //sinon execute les heurisitque Moyenne
             if (heuristiqueMoyenne(grille, n, liste, map) == 0)
             {
+                //si le niveau eneleve toutes les case retourne le niveau
                 retour = 2;
             }
             else
             {
                 if (level == 2)
                 {
+                    //sinon si le level choisi est celui la retourne que le niveau est dépassé
                     retour = 5;
                 }
                 else
                 {
-                    if (heuristiqueDifficile(grille, n, liste,map) == 0)
+                    //sinon execute les heurisitque Difficle
+                    if (heuristiqueDifficile(grille, n, liste, map) == 0)
                     {
+                        //si le niveau eneleve toutes les case retourne le niveau
                         retour = 3;
                     }
                     else
                     {
                         if (level == 3)
                         {
+                            //sinon si le level choisi est celui la retourne que le niveau est dépassé
                             retour = 5;
                         }
                         else
                         {
+                            //sinon si les heuristique n'on pas résolu l'algorithme mais qu'il est resolvable, alors retourne extreme.
                             retour = 4;
                         }
                     }
@@ -109,15 +120,15 @@ Liste* listeGenerer(size_t n,uint8_t **grille,uint8_t ** map)
 
     Liste * retour = NULL;
 
-    printf("taille : %i\n", taille);
-
     uint64_t *bbL = bitboard64Creer(taille);
     uint64_t *bbC = bitboard64Creer(taille);
     uint64_t *bbB = bitboard64Creer(taille);
+    //genere les bitboard, et si réussite alors
     if(bitBoardInitialiser(grille, n, bbL, bbC, bbB) == true){
+        //genere la premiere case de la liste, case mémoire vide.
         Liste * liste = NULL;
         Liste * temp = NULL;
-        uint64_t mask = (1<<taille) - 1;
+        uint64_t mask = (uint64_t)(1<<taille) - 1;
 
         liste = malloc(sizeof(Liste));
         liste->c = NULL;
@@ -136,7 +147,7 @@ Liste* listeGenerer(size_t n,uint8_t **grille,uint8_t ** map)
             {
                 if (grille[i][j] == 0)
                 {
-
+                    //pour toute case vide de la grille, genere des elements et les place a la suite de la liste.
                 	temp = malloc(sizeof(Liste));
                     temp->c = NULL;
                     temp->candidats = mask & (~(bbL[i] | bbC[j] | bbB[map[i][j]]));
@@ -157,17 +168,13 @@ Liste* listeGenerer(size_t n,uint8_t **grille,uint8_t ** map)
     return retour;
 }
 
-Liste32* listeGenerer32(size_t n, uint32_t *bbL, uint32_t *bbC, uint32_t *bbB, uint8_t **grille, uint8_t ** map)
+Liste32 * liste32Generer(size_t n,uint8_t **grille, uint32_t* bbL, uint32_t* bbC, uint32_t* bbB,uint8_t ** map)
 {
     size_t taille = n * n;
-
-    Liste32 * liste = NULL;
-    Liste32 * temp = NULL;
-
-    uint32_t mask = (1<<taille) - 1;
-
-    liste = malloc(sizeof(Liste32));
-    temp = liste;
+    Liste32 *retour = NULL;
+    Liste32 *prec = NULL;
+    Liste32 *liste = NULL;
+    uint32_t mask = (uint32_t)(1<<taille) - 1;
     for (size_t i = 0; i < taille; i++)
     {
         size_t tmp = (i / n) * n;
@@ -175,30 +182,41 @@ Liste32* listeGenerer32(size_t n, uint32_t *bbL, uint32_t *bbC, uint32_t *bbB, u
         {
             if (grille[i][j] == 0)
             {
-                temp->i = i;
-                temp->j = j;
-                temp->population = __builtin_popcount(temp->candidats);
-                temp->c = malloc(temp->population*sizeof(size_t));
+                //pour toute les cases du tableau vide, attribut les valeurs dans la liste
+                liste = malloc(sizeof(Liste32));
+                liste->candidats = mask & (~(bbL[i] | bbC[j] | bbB[map[i][j]]));
+                liste->i = i;
+                liste->j = j;
+                liste->suivante = NULL;
+                liste->population = __builtin_popcount(liste->candidats);
 
-                uint32_t candidat=temp->candidats;
-                size_t indice=0;
+                //donne la valeur a c
+                liste->c = malloc(liste->population*sizeof(size_t));
+
+                uint32_t candidat = liste->candidats;
+                size_t indice = 0;
                 size_t p = 0;
 
                 while(candidat)
                 {
                     indice = __builtin_ctz(candidat); //Cette méthode intégrée de GCC détermine le nombre de zéros de fin dans la représentation binaire d'un nombre.
-                    temp->c[p]= indice;
+                    liste->c[p]= indice;
                     candidat ^= (uint32_t)(1<<indice);
                     p++;
                 }       
-
-                temp->suivante = malloc(sizeof(Liste32));
-                temp->suivante->precedente = temp;
-                
-                temp = temp->suivante;
+                //attribut precedente et suivante.
+                liste->precedente = prec;
+                if(prec != NULL){
+                    prec->suivante = liste;
+                }
+                liste->suivante = NULL;
+                prec = liste;
+                if(retour == NULL){
+                    retour = liste;
+                }
+                liste = liste->suivante;
             }
         }
     }
-
-    return liste;
+    return retour;
 }
--- difficulte.h
+++ difficulte.h
@@ -39,10 +39,34 @@
 */
 int validiterEtDifficulter(uint8_t **grille, size_t taille, int level);
 
+/**
+ * @brief fonction qui permet de determiner la difficulté du sudoku en utilisant les heuristiques.
+ * @param uint8_t** grille du sudoku.
+ * @param size_t taille : taille du sudoku
+ * @param int level : niveau demandé par le jeu.
+ * @return int difficulte : retourne la difficulte de l'algorithme = {1 : Facile, 2: Moyen, 3: Difficile, 4: Expert, 5 : level dépassé}
+ */
 int difficulter(uint8_t **grille, size_t taille, int level);
 
+/**
+ * @brief fonction qui genere une liste 64 bits avec une en-tete pour mémoire. (premiere case a 0.)
+ * @param size_t n : taille d'un bloc.
+ * @param uint8_t** grille : grille du sudoku.
+ * @param uint8_t ** map : map des bloc du sudoku.
+ * @return Liste* : liste generer.
+ */
 Liste* listeGenerer(size_t n, uint8_t** grille,uint8_t ** map);
-Liste32* listeGenerer32(size_t n, uint32_t *bbL, uint32_t *bbC, uint32_t *bbB, uint8_t **grille,uint8_t ** map);
+/**
+ * @brief fonction qui genere une liste 32 bits pour la validiter.
+ * @param size_t n : taille d'un bloc.
+ * @param uint8_t** grille : grille du sudoku.
+ * @param uint32_t *bbL : bitboard des lignes.
+ * @param uint32_t *bbC : bitboard des colonnes.
+ * @param uint32_t *bbB : bitboard des bloc.
+ * @param uint8_t ** map : map des bloc du sudoku.
+ * @return Liste32* : liste generer.
+ */
+Liste32* liste32Generer(size_t n, uint8_t** grille,uint32_t *bbL, uint32_t *bbC, uint32_t *bbB,uint8_t ** map);
 
 
 #endif
